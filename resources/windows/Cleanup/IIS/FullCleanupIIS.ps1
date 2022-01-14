3e0b5ad434e507bb5c2bbd3205970e39111e0f4d
vrebyachih <vrebyachih@baltbet.ru>
2022-01-14 12:13:57 +0300

    fix verbosity

--- resources/windows/Cleanup/IIS/FullCleanupIIS.ps1
+++ resources/windows/Cleanup/IIS/FullCleanupIIS.ps1
@@ -4,5 +4,5 @@ Remove-Website -Name *
 Remove-WebAppPool -name *
 Stop-Service W3SVC
 Get-ChildItem 'C:\inetpub'  -Exclude custerr, history, logs, temp, wwwroot  | %{ 
-    "Removing file $($_.FullName)";Remove-Item -Force -Recurse }
+    "Removing file $($_.FullName)";Remove-Item -Force -Recurse -Path $_}
 Start-Service W3SVC
