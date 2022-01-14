07cc5f524878eea7fcb7d8831e735a53e27428e1
syudin <syudin@baltbet.ru>
2022-01-14 13:32:31 +0300

    Fix Pages.Prematch.IsHotEventsEnabled

--- resources/windows/Deploy/Uni/UniRuSql.ps1
+++ resources/windows/Deploy/Uni/UniRuSql.ps1
@@ -19,7 +19,6 @@ WHEN 'Global.WcfClient.WcfServicesHostAddress' THEN '#VM_IP'
 WHEN 'OAuth.LastLogoutUrl' THEN 'https://#VM_HOSTNAME.bb-webapps.com:449/account/logout/last'
 WHEN 'OAuth.TokenUrl' THEN 'https://#VM_HOSTNAME.bb-webapps.com:449/oauth/token'
 WHEN 'Global.RabbitMq.NotificationGateWayBus.IsEnabled' THEN 'false'
-WHEN 'Pages.Prematch.IsHotEventsEnabled' THEN 'true'
 ELSE Value END
 
 IF EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'Payment.IsCupisPaymentsEnabled')
@@ -28,6 +27,7 @@ IF EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'Payment.IsCupi
 ELSE INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
 	VALUES (1, 'Payment.IsCupisPaymentsEnabled', 'true', 0)
 
+	
 IF NOT EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'PlayerIdentificationSettings.ECupisAddressЕСИА')
 	INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
 	VALUES (1,N'PlayerIdentificationSettings.ECupisAddressЕСИА','https://wallet.1cupis.ru/auth',0)
@@ -45,7 +45,13 @@ IF NOT EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'Asterisk.L
 IF NOT EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'Asterisk.Secret')
 	INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
 	VALUES (1,'Asterisk.Secret','hzccIuSfo1rMgVBU',0)
+
+
+IF NOT EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'Pages.Prematch.IsHotEventsEnabled')
+	INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
+	VALUES (1,'Pages.Prematch.IsHotEventsEnabled','true',0)
 "
+
 $release_bak_folder = "\\server\tcbuild$\Testers\DB"
 $queryTimeout = 720
 
