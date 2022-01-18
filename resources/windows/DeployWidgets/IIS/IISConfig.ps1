768b646a3173a641923033f6f492481a78b8deeb
enesudimov <enesudimov@baltbet.ru>
2022-01-18 15:38:23 +0300

    Delete binding from BaltWidgets IIS

--- resources/windows/DeployWidgets/IIS/IISConfig.ps1
+++ resources/windows/DeployWidgets/IIS/IISConfig.ps1
@@ -13,7 +13,6 @@ $site = @{
         userName="$username";password="$pass";identitytype=3
         }
     Bindings= @(
-            @{protocol='http';bindingInformation="*:8880:"}
             @{protocol='https';;bindingInformation="$($env:COMPUTERNAME).$($wildcardDomain):450:"}
         )
     CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
