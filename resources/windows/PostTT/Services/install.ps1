c507b717cc6f07c8b3fe2a4860dca6873c44fade
vrebyachih <vrebyachih@baltbet.ru>
2022-02-02 19:37:09 +0300

    fix

--- resources/windows/PostTT/Services/install.ps1
+++ resources/windows/PostTT/Services/install.ps1
@@ -1,13 +1,13 @@
 import-module '.\scripts\sidefunctions.psm1'
 $doNotStart = @(
-"Baltbet.TradingTool.Scraping.Bet365",
-"Baltbet.TradingTool.Scraping.Betspan",
-"Baltbet.TradingTool.Scraping.Marathonbet",
-"Baltbet.TradingTool.Scraping.PinnacleApiDataReader",
-"Baltbet.TradingTool.Scraping.Sbobet",
-"Baltbet.TradingTool.Scraping.Surebet",
-"Baltbet.TradingTool.Scraping.Emulator",
-"Baltbet.TradingTool.Scraping.AllbestbetsApi",
+"Baltbet.TradingTool.Scraping.Bet365"
+"Baltbet.TradingTool.Scraping.Betspan"
+"Baltbet.TradingTool.Scraping.Marathonbet"
+"Baltbet.TradingTool.Scraping.PinnacleApiDataReader"
+"Baltbet.TradingTool.Scraping.Sbobet"
+"Baltbet.TradingTool.Scraping.Surebet"
+"Baltbet.TradingTool.Scraping.Emulator"
+"Baltbet.TradingTool.Scraping.AllbestbetsApi"
 )
 Get-ChildItem C:/Services/TradingTool -Exclude "tools","client"| Get-childitem -recurse -depth 1 -filter "*.exe" |? {
 	$_.fullname -notlike "*Baltbet.TradingTool.Api*" -and $_.fullname  -notlike "*chromedriver*"
