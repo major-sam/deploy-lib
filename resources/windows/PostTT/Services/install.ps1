c3ededd29a6632dbd60f336652d3b5a3d41b5a89
vrebyachih <vrebyachih@baltbet.ru>
2022-02-02 19:11:31 +0300

    fix startup

--- resources/windows/PostTT/Services/install.ps1
+++ resources/windows/PostTT/Services/install.ps1
@@ -1,9 +1,22 @@
 import-module '.\scripts\sidefunctions.psm1'
-
+$doNotStart = @(
+"Baltbet.TradingTool.Scraping.Bet365",
+"Baltbet.TradingTool.Scraping.Betspan",
+"Baltbet.TradingTool.Scraping.Marathonbet",
+"Baltbet.TradingTool.Scraping.PinnacleApiDataReader",
+"Baltbet.TradingTool.Scraping.Sbobet",
+"Baltbet.TradingTool.Scraping.Surebet",
+"Baltbet.TradingTool.Scraping.AllbestbetsApi",
+)
 Get-ChildItem C:/Services/TradingTool -Exclude "tools","client"| Get-childitem -recurse -depth 1 -filter "*.exe" |? {
 	$_.fullname -notlike "*Baltbet.TradingTool.Api*" -and $_.fullname  -notlike "*chromedriver*"
 	}| %{
 		$sname = registerwinservice($_)
-		start-service $sname
-		Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
+		if ($sname -in $doNotStart){
+			write-out "$sname autostart disabled. Use manual startuo"
+		}
+		else{
+			start-service $sname
+			Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
+		}
 	}
