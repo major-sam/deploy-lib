a80c10b63c859d6e5e10c01d4baab6aff1372a2d
vrebyachih <vrebyachih@baltbet.ru>
2022-02-02 19:54:29 +0300

    fix

--- resources/windows/Deploy/PromoExpressService/db.ps1
+++ resources/windows/Deploy/PromoExpressService/db.ps1
@@ -1,7 +1,12 @@
 Import-module '.\scripts\sideFunctions.psm1'
 
 $dbname = "PromoExpressService"
+$initScript = 'c:\services\PromoExpressService\init.sql' 
 
 CreateSqlDatabase $dbname
-
-Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile  'c:\services\PromoExpressService\init.sql' -ErrorAction continue
+if (test-Path $initScript){
+	Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $initScript -ErrorAction continue
+}
+else{
+	write-host "$initScript  not found"
+}
