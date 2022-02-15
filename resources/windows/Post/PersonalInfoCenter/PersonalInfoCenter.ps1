2aa6a97a4e3cc12299203500c31cdc4836b895bd
vrebyachih <vrebyachih@baltbet.ru>
2022-02-15 14:57:30 +0300

    fix

--- resources/windows/Post/PersonalInfoCenter/PersonalInfoCenter.ps1
+++ resources/windows/Post/PersonalInfoCenter/PersonalInfoCenter.ps1
@@ -1,8 +1,8 @@
 import-module '.\scripts\sideFunctions.psm1'
 $picRoot = 'c:\Services\PersonalInfoCenter'
 $serviceBins = @(
-		Get-ChildItem -Recurse -Filter *.exe -Depth 2 $picRoot |%  {$_.fullname} | ?
-			{$_ -inotlike "*AdminMessageService*"}
+		Get-ChildItem -Recurse -Filter *.exe -Depth 2 $picRoot |%  {$_.fullname} |? {
+			$_ -inotlike "*AdminMessageService*"}
 )
 $serviceBins | % {
 	$sname = RegisterWinService(get-item -path $_)
