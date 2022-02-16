f4087069a4a83487dd2f71c24281e5c9c7fe4fa9
vrebyachih <vrebyachih@baltbet.ru>
2022-02-16 15:40:01 +0300

    fix kernel start

--- resources/windows/Post/Kernel/RegisterService.ps1
+++ resources/windows/Post/Kernel/RegisterService.ps1
@@ -2,7 +2,7 @@ import-module '.\scripts\sideFunctions.psm1'
 
 $serviceBin = Get-Item  "C:\Kernel\Kernel.exe"
 $sname = RegisterWinService($serviceBin)
-start-Service $sname
+(get-service $sname).start()
 Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
 $serviceBin = Get-Item  "C:\KernelWeb\KernelWeb.exe"
 $sname = RegisterWinService($serviceBin)
