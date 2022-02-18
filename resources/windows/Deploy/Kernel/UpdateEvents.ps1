aa59f6ef6711ed84ad9ac591abf411bb20412617
vrebyachih <vrebyachih@baltbet.ru>
2022-02-18 14:20:57 +0300

    add update events script

---
+++ resources/windows/Deploy/Kernel/UpdateEvents.ps1
@@ -0,0 +1,17 @@
+$q  = @'DECLARE @MinDate datetime
+
+SELECT @MinDate = MIN(EventStartTime) FROM EVENTS WHERE LineID <>1
+
+DECLARE @DateDiff int
+SET @DateDiff = datediff(DAY, @MinDate, getdate())
+SELECT @DateDiff
+
+UPDATE Events SET 
+EventCreationTime = DATEADD(DAY, @DateDiff, EventCreationTime),
+EventStartTime = DATEADD(DAY, @DateDiff, EventStartTime),
+BetStartDate = DATEADD(DAY, @DateDiff, BetStartDate),
+BetEndDate = DATEADD(DAY, @DateDiff, BetEndDate)
+WHERE LineID <>1
+'@
+
+Invoke-Sqlcmd -verbose -ServerInstance $env:COMPUTERNAME -Database 'BaltVBetM' -query $q -ErrorAction Stop
