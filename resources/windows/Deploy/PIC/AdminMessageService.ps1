11ee250725175f07dc5a3617f480a1d6fa024d5c
vrebyachih <vrebyachih@baltbet.ru>
2022-02-22 15:56:00 +0300

    update AdminMessage service

--- resources/windows/Deploy/PIC/AdminMessageService.ps1
+++ resources/windows/Deploy/PIC/AdminMessageService.ps1
@@ -5,6 +5,15 @@ $webdoc = [Xml](Get-Content $svc.Fullname)
 $webdoc.log4net.appender.file.value = "c:\logs\PersonalInfoCenter\$($svc.Directory.name)-"
 $webdoc.Save($svc.Fullname)
 
+
+$pathtojson = "C:\Services\PersonalInfoCenter\AdminMessageService\appsettings.json"
+$config = Get-Content -Path $pathtojson -Encoding UTF8
+$json_appsetings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
+
+$json_appsetings.CKFinder.Url = "https://$($env:COMPUTERNAME):44307/"
+$json_appsetings.Authorization.Realm = "https://$($env:COMPUTERNAME):44307/"
+ConvertTo-Json $json_appsetings -Depth 4  | Format-Json | Set-Content $pathtojson -Encoding UTF8
+
 $reportval =@"
 [AdminMessageService]
 $config
