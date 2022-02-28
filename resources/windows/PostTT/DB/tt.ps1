a806113effb28adae23676971cb359f1ab38e9a2
vrebyachih <vrebyachih@baltbet.ru>
2022-02-28 15:56:15 +0300

    add tt proxy

--- resources/windows/PostTT/DB/tt.ps1
+++ resources/windows/PostTT/DB/tt.ps1
@@ -8,11 +8,46 @@ Where [key] = 'IMPORT_SERVICE_URL'"
 $q0 ="
 Insert into trading.Proxies (AddedDate, Uri, Username, Password)
 Values 
+(getdate(),'31.28.11.66:41411','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41412','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41413','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41414','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41415','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41416','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41417','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41418','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41419','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41420','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41421','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41422','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41423','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41424','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41425','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41426','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41427','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41428','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41429','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41430','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41431','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41432','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41433','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41434','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41435','snekrasov','aPbj5kB'),
 (getdate(),'31.28.11.66:41436','snekrasov','aPbj5kB'),
 (getdate(),'31.28.11.66:41437','snekrasov','aPbj5kB'),
 (getdate(),'31.28.11.66:41438','snekrasov','aPbj5kB'),
 (getdate(),'31.28.11.66:41439','snekrasov','aPbj5kB'),
-(getdate(),'31.28.11.66:41440','snekrasov','aPbj5kB')"
+(getdate(),'31.28.11.66:41440','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41441','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41442','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41443','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41444','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41445','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41446','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41447','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41448','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41449','snekrasov','aPbj5kB'),
+(getdate(),'31.28.11.66:41450','snekrasov','aPbj5kB')"
 $q1 ="
 INSERT INTO [trading].[ProxyServices]
 SELECT 
