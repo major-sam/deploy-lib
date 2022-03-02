182a5e42d109dc302ea71c363c6f7f52c7941461
syudin <syudin@baltbet.ru>
2022-03-02 12:27:51 +0300

    Add exit code to CurrencyConverter sql section

--- resources/windows/Deploy/CurrencyConverter/CurrencyConverterDB.ps1
+++ resources/windows/Deploy/CurrencyConverter/CurrencyConverterDB.ps1
@@ -8,13 +8,23 @@ $sqlTasksFolder = "C:\Services\CurrencyConverter\CurrencyConverter.DB\Tasks"
 CreateSqlDatabase($dbname)
 
 # Выполняем скрипт инициализации
-foreach ($script in (Get-Item -Path $initScriptFolder\* -Include "*.sql").FullName | Sort-Object ) {    
-    Write-Host -ForegroundColor Green "[INFO] Execute $script on $dbname"
-    Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+$initScript = "${initScriptFolder}\Init.sql"
+if(Test-Path $initScript) {
+    foreach ($script in (Get-Item -Path $initScriptFolder\* -Include "*.sql").FullName | Sort-Object ) {    
+        Write-Host -ForegroundColor Green "[INFO] Execute $script on $dbname"
+        Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+    }
+} else {
+    Write-Host -ForegroundColor Green "[WARN] There is no ${initScript} "
+    exit 1
 }
 
 # Выполняем скрипт по задаче
-foreach ($script in (Get-Item -Path $sqlTasksFolder\* -Include "*.sql").FullName | Sort-Object ) {    
-    Write-Host -ForegroundColor Green "[INFO] Execute $script on $dbname"
-    Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+if (Test-Path $sqlTasksFolder) {
+    foreach ($script in (Get-Item -Path $sqlTasksFolder\* -Include "*.sql").FullName | Sort-Object ) {    
+        Write-Host -ForegroundColor Green "[INFO] Execute $script on $dbname"
+        Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+    }    
+} else {
+    Write-Host -ForegroundColor Green "[INFO] There is no Tasks folder"
 }
