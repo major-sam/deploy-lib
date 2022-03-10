7bf9bfc3432aefac7d2249275a2bca5df1937c5b
syudin <syudin@baltbet.ru>
2022-03-10 16:35:04 +0300

    Add changing connectionString

--- resources/windows/Deploy/UpdateApkService/UpdateApkService.ps1
+++ resources/windows/Deploy/UpdateApkService/UpdateApkService.ps1
@@ -2,6 +2,8 @@ Write-Host -ForegroundColor Green "[INFO] Deploy UpdateApkService..."
 
 $ApkCacheFolder = "C:\ApkCacheFolder"
 $configFile = "C:\Services\UpdateApk\UpdateApkService\Web.config"
+$dbname = "UpdateApkService"
+$connectionString = "Data Source=localhost;Initial Catalog=${dbname};Integrated Security=true;MultipleActiveResultSets=True;"
 
 # Прверяем существует ли директория, если нет - создаем
 if(Test-Path -Path $ApkCacheFolder) {
@@ -15,4 +17,5 @@ if(Test-Path -Path $ApkCacheFolder) {
 $config = [xml](Get-Content -Path $configFile -Encoding utf8)
 Write-Host -ForegroundColor Green "[INFO] Change ApkCacheFolder.value to $ApkCacheFolder "
 ($config.configuration.appSettings.add | Where-Object key -eq "apkCacheFolder").value = "$ApkCacheFolder"
+($config.configuration.connectionStrings.add | Where-Object name -eq "UpdateApkContext").connectionString = "$connectionString"
 $config.Save($configFile)
