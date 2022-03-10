c32c5d8fd5da0b1dc8b04a17b5a2a2054e0424bb
syudin <syudin@baltbet.ru>
2022-03-10 16:14:25 +0300

    Add UpdateApkService

---
+++ resources/windows/Deploy/UpdateApkService/UpdateApkServiceDB.ps1
@@ -0,0 +1,27 @@
+Import-module '.\scripts\sideFunctions.psm1'
+
+$dbname = "UpdateApk"
+$initScriptFolder = "C:\Services\UpdateApk\DB"
+
+
+# Создаем БД UpdateApk
+CreateSqlDatabase($dbname)
+
+# Выполняем скрипт инициализации
+$initScript = "${initScriptFolder}\InitDb.sql"
+if(Test-Path $initScript) {
+    Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+} else {
+    Write-Host -ForegroundColor Green "[WARN] There is no ${initScript} "
+    #exit 1
+}
+
+# Выполняем остальные скрипты
+if (Test-Path $initScriptFolder) {
+    foreach ($script in (Get-Item -Path $initScriptFolder\* -Include "*.sql" -Exclude "InitDb.sql").FullName | Sort-Object ) {    
+        Write-Host -ForegroundColor Green "[INFO] Execute $script on $dbname"
+        Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database $dbname -InputFile $script -ErrorAction continue
+    }    
+} else {
+    Write-Host -ForegroundColor Green "[INFO] There is no Tasks folder"
+}
