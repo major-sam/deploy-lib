3c85b7f5901929e6420098271b239459a7ced446
vrebyachih <vrebyachih@baltbet.ru>
2022-03-15 11:56:07 +0300

    add sort

--- resources/windows/Post/Kernel/Kernel.ps1
+++ resources/windows/Post/Kernel/Kernel.ps1
@@ -1,6 +1,6 @@
 if (test-path "c:\kernel\TasksDB"){
 	write-host "[INFO] Exec tasks"
-	get-ChildItem "c:\Kernel\TasksDB\*" -include "*.sql" | % {
+	get-ChildItem "c:\Kernel\TasksDB\*" -include "*.sql" | Sort-Object | % {
 		Write-Host -ForegroundColor Green "[INFO] Execute script $_ on database BaltBetM"
 		Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Database 'BaltBetM' -InputFile $_ -ErrorAction continue
 		Write-Host -ForegroundColor Green "[INFO] Execute script $_ on database BaltBetMMirror"
