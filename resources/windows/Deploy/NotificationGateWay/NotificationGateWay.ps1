cd5bdcdd2c669d27e5fee3c24aefebc9e894d80e
enesudimov <enesudimov@baltbet.ru>
2022-04-01 14:27:52 +0300

    NotificationGateWay: fix config changes

--- resources/windows/Deploy/NotificationGateWay/NotificationGateWay.ps1
+++ resources/windows/Deploy/NotificationGateWay/NotificationGateWay.ps1
@@ -18,18 +18,18 @@ $jsonAppsetings.AppSettings.EmailSettings.Default.FromLogin = "reg@baltbet.ru"
 $jsonAppsetings.AppSettings.EmailSettings.Default.FromPass = "iFbFWtSjeL1"
 
 # Настраиваем email для com
-$jsonAppsetings.AppSettings.Emails[0].Default.From = "pay230@baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[0].Default.FromDisplayName = "TEST"
-$jsonAppsetings.AppSettings.Emails[0].Default.FromHost = "smtp.baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[0].Default.FromLogin = "pay230@baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[0].Default.FromPass = "qk`oP(%;KW'J/Pp"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[0].From = "pay230@baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[0].FromDisplayName = "TEST"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[0].FromHost = "smtp.baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[0].FromLogin = "pay230@baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[0].FromPass = "qk`oP(%;KW'J/Pp"
 
 # Настраиваем email для ru
-$jsonAppsetings.AppSettings.Emails[1].Default.From = "nkiryushina@baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[1].Default.FromDisplayName = "TEST"
-$jsonAppsetings.AppSettings.Emails[1].Default.FromHost = "smtp.baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[1].Default.FromLogin = "nkiryushina@baltbet.ru"
-$jsonAppsetings.AppSettings.Emails[1].Default.FromPass = "ПАРОЛЬ"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[1].From = "nkiryushina@baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[1].FromDisplayName = "TEST"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[1].FromHost = "smtp.baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[1].FromLogin = "nkiryushina@baltbet.ru"
+$jsonAppsetings.AppSettings.EmailSettings.Emails[1].FromPass = "ПАРОЛЬ"
 
 # Настраиваем коннект к БД сервиса
 $jsonAppsetings.ConnectionStrings.DbConnectionString = "Server=$($env.COMPUTERNAME);Database=BaltBet.SmsService.Db;Trusted_Connection=True;"
