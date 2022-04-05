cffd6f3f64534993a6888f88f0deb59dcdcb6821
enesudimov <enesudimov@baltbet.ru>
2022-04-05 15:51:26 +0300

    fix UniAuthService

--- resources/windows/Post/UniAuthService/UniAuthService.ps1
+++ resources/windows/Post/UniAuthService/UniAuthService.ps1
@@ -18,7 +18,7 @@ $WebApiConfig = "C:\inetpub\ClientWorkPlace\uniruwebapi\Web.config"
 $webdoc = [Xml](Get-Content $WebApiConfig)
 $logoutService = $webdoc.configuration.Grpc.Services.add | Where-Object name -eq "LogoutServiceClient"
 $logoutService.host = $IPAddress
-$logoutService.port = 5307
+$logoutService.port = "5307"
 
 $authUrl = $webdoc.configuration.connectionStrings.add | Where-Object name -eq "UniAuthServiceUrl"
 $authUrl.connectionString = "https://$($env:COMPUTERNAME).bb-webapps.com:450"
@@ -31,5 +31,5 @@ $WebsiteRuConfig = "C:\inetpub\Website\Web.config"
 $webdoc = [Xml](Get-Content $WebsiteRuConfig)
 $logoutService = $webdoc.configuration.Grpc.Services.add | Where-Object name = "LogoutServiceClient"
 $logoutService.host = $IPAddress
-$logoutService.port = 5307
+$logoutService.port = "5307"
 $webdoc.Save($WebsiteRuConfig)
