ab43502e4e64e91653d0c0bd93421a61e7d4f5da
enesudimov <enesudimov@baltbet.ru>
2022-04-05 15:23:26 +0300

    Fix UniAuthService deploy

--- resources/windows/Deploy/UniAuthService/UniAuthServiceGrpc.ps1
+++ resources/windows/Deploy/UniAuthService/UniAuthServiceGrpc.ps1
@@ -7,13 +7,13 @@ UniAuthServiceGrpc (запускаем как windows сервис)
 
 Import-module '.\scripts\sideFunctions.psm1'
 
-
+$IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
 $serviceName = "UniAuthServiceGrpc"
 $targetDir = "C:\Services\UniAuthService\${serviceName}"
 $ProgressPreference = 'SilentlyContinue'
 $pathtojson = "$targetDir\appSettings.json"
 $jsonDepth = 5
-$uasGrpcPort = 5037
+$uasGrpcPort = 5307
 $dbname = "AuthService"
 
 Write-host "[INFO] Start ${serviceName} deploy script"
@@ -24,7 +24,7 @@ $jsonAppsetings = Get-Content -Raw -path $pathtojson  | % { $_ -replace '[\s^]//
 $jsonAppsetings.ConnectionStrings.AuthDb = "data source=localhost;initial catalog=${dbname};Integrated Security=SSPI;MultipleActiveResultSets=True;"
 
 # Меняем порт GrpcPort
-$jsonAppsetings.Kestrel.Endpoints.Grpc.Url = "http://0.0.0.0:${uasGrpcPort}"
+$jsonAppsetings.Kestrel.Endpoints.Grpc.Url = "http://$($IPAddress):${uasGrpcPort}"
 
 ConvertTo-Json $jsonAppsetings -Depth $jsonDepth  | Format-Json | Set-Content $pathtojson -Encoding UTF8
 Write-Host -ForegroundColor Green "[INFO] $pathtojson renewed with json depth $jsonDepth"
