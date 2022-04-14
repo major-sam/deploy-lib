627c8d46709a89890bdad72f93637dcae2ca9ea9
vrebyachih <vrebyachih@baltbet.ru>
2022-04-14 11:12:17 +0300

    fix

--- resources/linux/Deploy/redis/redis.sh
+++ resources/linux/Deploy/redis/redis.sh
@@ -7,16 +7,16 @@ echo $REDIS_PORT
 echo '==============================='
 
 
-/usr/sbin/helm upgrade -i redis bitnami/redis \
-    --version 16.5.2 \
-    --namespace $NAMESPACE \
-    --create-namespace \
-    --set master.service.nodePorts.redis=$REDIS_PORT \
-    --set master.service.type="NodePort" \
-    --set auth.enabled="false" \
-    --set replica.replicaCount=0  \
-    --set master.resources.requests.cpu=250m \
-    --set master.resources.limits.cpu=250m   \
-    --set master.resources.requests.memory=250Mi \
-    --set master.resources.limits.memory=250Mi
-
+##/usr/sbin/helm upgrade -i redis bitnami/redis \
+##    --version 16.5.2 \
+##    --namespace $NAMESPACE \
+##    --create-namespace \
+##    --set master.service.nodePorts.redis=$REDIS_PORT \
+##    --set master.service.type="NodePort" \
+##    --set auth.enabled="false" \
+##    --set replica.replicaCount=0  \
+##    --set master.resources.requests.cpu=250m \
+##    --set master.resources.limits.cpu=250m   \
+##    --set master.resources.requests.memory=250Mi \
+##    --set master.resources.limits.memory=250Mi
+##
