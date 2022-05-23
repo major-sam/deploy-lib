2661d91dde0d1b99d2586c5bf97c65eceb893b44
vrebyachih <vrebyachih@baltbet.ru>
2022-05-23 14:12:43 +0300

    fix uni

--- resources/windows/Deploy/Uni/UniRu.ps1
+++ resources/windows/Deploy/Uni/UniRu.ps1
@@ -5,7 +5,7 @@ Import-module '.\scripts\sideFunctions.psm1'
 $targetDir = "C:\inetpub\ClientWorkPlace\UniRu"
 $webConfig = "$targetDir\Web.config"
 $IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
-$redispasswd = "$($ENV:REDIS_CREDS_PSW)$($ENV:VM_ID)" 
+$redispasswd = "$($ENV:REDIS_CREDS_PSW)$($ENV:VM_ID)"
 $redisPwdStr= "password=$redispasswd"
 $shortRedisStr="$($env:REDIS_HOST):$($env:REDIS_Port),$redisPwdStr"
 ###
@@ -13,35 +13,29 @@ $shortRedisStr="$($env:REDIS_HOST):$($env:REDIS_Port),$redisPwdStr"
 ####
 Write-Host -ForegroundColor Green "[INFO] Edit web.config of $webConfig"
 $webdoc = [Xml](Get-Content $webConfig)
-($webdoc.configuration.connectionStrings.add | where {
-	$_.name -eq 'DataContext' 
+($webdoc.configuration.connectionStrings.add | where { $_.name -eq 'DataContext'
 	}).connectionString = "data source=localhost;initial catalog=UniRu;Integrated Security=true;MultipleActiveResultSets=True;"
-($webdoc.configuration.connectionStrings.add | where {
-	$_.name -eq 'UniPaymentsServiceUrl' 
+($webdoc.configuration.connectionStrings.add | where { $_.name -eq 'UniPaymentsServiceUrl'
 	}).connectionString = "https://${env:COMPUTERNAME}.bb-webapps.com:54381".ToLower()
-$webdoc.configuration.connectionStrings.add | % {
-	if ($_.name -eq 'UniBonusServiceUrl' ){
-		$_.connectionString = "https://${env:COMPUTERNAME}.bb-webapps.com:4437".ToLower()
-	}
-}
+($webdoc.configuration.connectionStrings.add | where { $_.name -eq 'UniBonusServiceUrl'
+	}).connectionString = "https://${env:COMPUTERNAME}.bb-webapps.com:4437".ToLower()
 Write-Host 'REDIS CONFIG'
-$webdoc.configuration.connectionStrings.add | % {
-	if ($_.name -eq "Redis") { $_.connectionString = $shortRedisStr } 
+if ($webdoc.configuration.connectionStrings.add | where { $_.name -eq "Redis"}){
+	($webdoc.configuration.connectionStrings.add | where { $_.name -eq "Redis"}).connectionString = $shortRedisStr 
 }
 $webdoc.configuration."system.web".sessionState.providers.add.connectionString = "$shortRedisStr,syncTimeout=10000,allowAdmin=True,connectTimeout=50000"
 $webdoc.configuration."system.web".sessionState.providers.add.accessKey = $redispasswd
 $webdoc.configuration.cache.redis.connection = "$shortRedisStr,syncTimeout=10000,allowAdmin=True,connectTimeout=50000,ssl=False,abortConnect=False,connectRetry=10,proxy=None,configCheckSeconds=5"
-($webdoc.configuration.connectionStrings.add | where {
-	$_.name -eq 'UniEventServiceUrl' 
+($webdoc.configuration.connectionStrings.add | where { $_.name -eq 'UniEventServiceUrl'
 	}).connectionString = "https://${env:COMPUTERNAME}.bb-webapps.com:4435".ToLower()
 $webdoc.configuration.cache.db.connection = "data source=localhost;initial catalog=UniRu;Integrated Security=true;MultipleActiveResultSets=True;"
 ($webdoc.configuration.Grpc.services.add | where {$_.name -eq 'DefaultService' }).host = $IPAddress
 ($webdoc.configuration.Grpc.services.add | where {$_.name -eq 'PromocodeAdminService' }).host = $IPAddress
-#configuration.appSettings.SelectNodes add node enable swagger   
+#configuration.appSettings.SelectNodes add node enable swagger
 $targetNode = $webdoc.configuration.appSettings.SelectNodes("add[@key='webapi:EnableSwagger']")
 if($targetNode.Count){
-    write-host 'remove old nodes'
-    foreach($node in $targetNode){$node.ParentNode.RemoveChild($node)}
+	write-host 'remove old nodes'
+	foreach($node in $targetNode){$node.ParentNode.RemoveChild($node)}
 }
 write-host 'creating node'
 $webdoc.configuration.appSettings.add| fc
@@ -49,13 +43,16 @@ $new = $webdoc.CreateElement("add")
 $new.SetAttribute("key","webapi:EnableSwagger")
 $new.SetAttribute( "value","false")
 if (!$webdoc.configuration.appSettings.add.Contains($new)){
-		$webdoc.configuration.appSettings.AppendChild($new)
+	$webdoc.configuration.appSettings.AppendChild($new)
 }
-(($webdoc.configuration.ckfinder.backends.backend|	where {
-	$_.name -ilike "default"
-	}).option| where {
-		$_.name -ilike 'root'}).value = 'c:\inetpub\images'
+if ($webdoc.configuration.ckfinder){
+		(($webdoc.configuration.ckfinder.backends.backend|	where {
+			$_.name -ilike "default" }).option| where {
+				$_.name -ilike 'root'}).value = 'c:\inetpub\images'
+}
+else{
+	write-host 'CKFinder moved to admin app'
+}
 $webdoc.Save($webConfig)
 
 Write-Host -ForegroundColor Green "[INFO] Done"
-
