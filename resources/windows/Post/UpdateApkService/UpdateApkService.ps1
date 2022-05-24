64ef0f36c14d443800a93cf1e875273db423c746
syudin <syudin@baltbet.ru>
2022-05-24 14:08:59 +0300

    Change db settngs for UpdateApkService

--- resources/windows/Post/UpdateApkService/UpdateApkService.ps1
+++ resources/windows/Post/UpdateApkService/UpdateApkService.ps1
@@ -4,18 +4,18 @@ Id	GroupId	Name	Value	IsInherited
 353	1	UpdateApk.CacheExpiry	0.0:0:40.0	0
 #>
 
-$db = "UniRu"
+$db = "UniAdministration"
 $q = "
-IF EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'UpdateApk.CheckUrl')
-    UPDATE UniRu.Settings.SiteOptions SET Value = 'https://#VM_HOSTNAME.bb-webapps.com:4458/api/check/'
+IF EXISTS (SELECT * FROM UniAdministration.Settings.SiteOptions	WHERE Name = 'UpdateApk.CheckUrl')
+    UPDATE UniAdministration.Settings.SiteOptions SET Value = 'https://#VM_HOSTNAME.bb-webapps.com:4458/api/check/'
 	    WHERE Name = 'UpdateApk.CheckUrl'
-ELSE INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
+ELSE INSERT INTO UniAdministration.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
 	VALUES (1, 'UpdateApk.CheckUrl', 'https://#VM_HOSTNAME.bb-webapps.com:4458/api/check/', 0)
 
-IF EXISTS (SELECT * FROM UniRu.Settings.SiteOptions	WHERE Name = 'UpdateApk.CacheExpiry')
-    UPDATE UniRu.Settings.SiteOptions SET Value = '0.0:0:40.0'
+IF EXISTS (SELECT * FROM UniAdministration.Settings.SiteOptions	WHERE Name = 'UpdateApk.CacheExpiry')
+    UPDATE UniAdministration.Settings.SiteOptions SET Value = '0.0:0:40.0'
 	    WHERE Name = 'UpdateApk.CacheExpiry'
-ELSE INSERT INTO UniRu.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
+ELSE INSERT INTO UniAdministration.Settings.SiteOptions (GroupId, Name, Value, IsInherited)
 	VALUES (1, 'UpdateApk.CacheExpiry', '0.0:0:40.0', 0)
 "
 
