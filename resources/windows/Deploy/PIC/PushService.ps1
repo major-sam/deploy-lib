ca55219ef4c89d6a60273f2c732c58e073f4d98f
vrebyachih <vrebyachih@baltbet.ru>
2022-05-27 18:55:24 +0300

    update push service (sqlproj migration)

--- resources/windows/Deploy/PIC/PushService.ps1
+++ resources/windows/Deploy/PIC/PushService.ps1
@@ -1,19 +1,35 @@
 import-module '.\scripts\sideFunctions.psm1'
-$rabbitpasswd = "$($env:RABBIT_CREDS_PSW)$($ENV:VM_ID)" 
+$rabbitpasswd = "$($env:RABBIT_CREDS_PSW)$($ENV:VM_ID)"
 $shortRabbitStr="host=$($ENV:RABBIT_HOST):$($ENV:RABBIT_PORT);username=$($ENV:RABBIT_CREDS_USR);password=$rabbitpasswd"
 
 $config = 	"C:\Services\PersonalInfoCenter\PushService\Log.config"
 if (test-path $config){
-        $svc = get-item $config 
+        $svc = get-item $config
         $webdoc = [Xml](Get-Content $svc.Fullname)
         $webdoc.log4net.appender.file.value = "c:\logs\PersonalInfoCenter\$($svc.Directory.name)-"
         $webdoc.Save($svc.Fullname)
 }
 else{write-host  $config ' not exists'        }
 
-CreateSqlDatabase ("PushService")
-$file =	"C:\Services\PersonalInfoCenter\PushServiceDB\init.sql"
-Invoke-Sqlcmd -ServerInstance $env:COMPUTERNAME -Database "PushService" -InputFile $file -Verbose
+$file ="C:\Services\PersonalInfoCenter\PushServiceDB\init.sql"
+$backup ="C:\Services\PersonalInfoCenter\PushServiceDB\init.bak"
+if (test-path $file){
+        CreateSqlDatabase ("PushService")
+        Invoke-Sqlcmd -ServerInstance $env:COMPUTERNAME -Database "PushService" -InputFile $file -Verbose
+}
+elseif (test-path $backup){
+        $dbs = @(
+                        @{
+                        DbName = "PushService"
+                        BackupFile = $backup
+                        }
+                )
+Write-Host -ForegroundColor Green "[INFO] Create dbs"
+
+RestoreSqlDb -db_params $dbs
+
+}
+else{ write-host 'No DB found'}
 
 $pathtojson = "C:\Services\PersonalInfoCenter\PushService\appsettings.json"
 
