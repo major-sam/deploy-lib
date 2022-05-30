bd00ca7760cd631adb33d048226c7a22fad1a0b2
syudin <syudin@baltbet.ru>
2022-05-30 11:51:29 +0300

    Add new PSH

--- resources/windows/Deploy/PaymenSystemHandlers/PaymenSystemHandlers.ps1
+++ resources/windows/Deploy/PaymenSystemHandlers/PaymenSystemHandlers.ps1
@@ -1,9 +1,42 @@
-$IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1 }).IPAddress.trim()
-$webConfig = 'c:\inetpub\PaymenSystemHandlers\Web.config'
-$webdoc = [Xml](Get-Content $webConfig)
-$webdoc.configuration.log4net.appender.file.value = "c:\logs\Payments\Payment-handlers\"
-$webdoc.configuration.appSettings.add| %{if ($_.key -like "ServerAddress"){
-	$_.value =	"$($IPAddress):8082"}
-}
-$webdoc.Save($webConfig)
+# PAY-757
+# https://jira.baltbet.ru:8443/browse/PAY-757
+# Перевод сервиса PSH на GRPC ASAP
 
+Import-module '.\scripts\sideFunctions.psm1'
+
+
+$ServiceName = "PaymenSystemHandlers"
+$pshKestrelPort = 7149
+$kernelGrpcPort = 32419
+$defaultDomain = "bb-webapps.com"
+$pacThumbprint = $env:PAC_CERT_THUMBPRINT
+
+# Редактируем конфиг
+Write-Host -ForegroundColor Green "[INFO] Edit ${ServiceName} configuration files..."
+$pathtojson = "C:\Services\${ServiceName}\appsettings.json"
+$config = Get-Content -Path $pathtojson -Encoding UTF8
+$json_appsetings = $config -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/' | ConvertFrom-Json
+
+# Правим путь к лог файлу
+Write-Host -ForegroundColor Green "[INFO] Change Serilog.WriteTo..."
+$json_appsetings.Serilog.WriteTo | % { if ($_.Name -like 'File') {
+        $_.Args.path = "C:\Logs\${ServiceName}\${ServiceName}-.log"   
+    }
+}
+
+# Правим настройки Kestrel
+Write-Host -ForegroundColor Green "[INFO] Change Kestrel.Endpoints..."
+$json_appsetings.Kestrel.Endpoints.Https.Url = "https://$($env:COMPUTERNAME).$($defaultDomain):${pshKestrelPort}/"
+$json_appsetings.Kestrel.Endpoints.Https.Certificate.Subject = "*.bb-webapps.com"
+
+# Правим настройки KernelGrpcOptions
+Write-Host -ForegroundColor Green "[INFO] Change KernelGrpcOptions..."
+$json_appsetings.KernelGrpcOptions.Url = "http://$($env:COMPUTERNAME).$($defaultDomain):${kernelGrpcPort}/"
+
+# Правим настройки AggregatorAuthOptions
+Write-Host -ForegroundColor Green "[INFO] Change AggregatorAuthOptions..."
+$json_appsetings.AggregatorAuthOptions.CertificateThumbprint = "$pacThumbprint"
+$json_appsetings.AggregatorAuthOptions.ValidOnly = $false
+
+
+ConvertTo-Json $json_appsetings -Depth 4 | Format-Json | Set-Content $pathtojson -Encoding UTF8
---
+++ resources/windows/Post/PaymenSystemHandlers/PaymenSystemHandlers.ps1
@@ -0,0 +1,20 @@
+# PAY-757
+# https://jira.baltbet.ru:8443/browse/PAY-757
+# Перевод сервиса PSH на GRPC ASAP
+
+Import-module '.\scripts\sideFunctions.psm1'
+
+
+$ServiceName = "PaymenSystemHandlers"
+
+# Регистрируем сервис
+$serviceBinPath = "C:\Services\$ServiceName\PaymentSystemHandlers.exe"
+if (test-path $serviceBinPath) {
+    $serviceBin = Get-Item $serviceBinPath
+    $sname = RegisterWinService($serviceBin)
+    Start-Service $sname
+    Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
+}
+else {
+    write-host "[WARN] File $serviceBinPath does not exist"
+}
