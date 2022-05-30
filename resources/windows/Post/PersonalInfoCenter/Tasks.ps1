fa0f3d788f68e12e13ad90d4f356e95e43dc5306
vrebyachih <vrebyachih@baltbet.ru>
2022-05-30 12:37:47 +0300

    MSG-263 update for PIC

--- resources/windows/Post/PersonalInfoCenter/Tasks.ps1
+++ resources/windows/Post/PersonalInfoCenter/Tasks.ps1
@@ -1,5 +1,15 @@
 Import-module '.\scripts\sideFunctions.psm1'
 
-$folder = "C:\Services\PersonalInfoCenter\MessageService\DB"
 $dbname = 'MessageService' 
-ApplyTasks -ScriptFolder $folder -TargetDB $dbname -DBServer $env:COMPUTERNAME
+if(Test-Path C:\Services\PersonalInfoCenter\MessageServiceDb\Tasks\*){
+	Write-host 'apply db Tasks'
+	$folder = "C:\Services\PersonalInfoCenter\MessageService\DB"
+	ApplyTasks -ScriptFolder $folder -TargetDB $dbname -DBServer $env:COMPUTERNAME
+}elseif(Test-Path C:\Services\PersonalInfoCenter\MessageService\DB)
+	Write-host 'apply old db Tasks'
+	$folder = "C:\Services\PersonalInfoCenter\MessageService\DB"
+	ApplyTasks -ScriptFolder $folder -TargetDB $dbname -DBServer $env:COMPUTERNAME
+}
+}else
+	Write-host 'No db Tasks for issue'
+}
