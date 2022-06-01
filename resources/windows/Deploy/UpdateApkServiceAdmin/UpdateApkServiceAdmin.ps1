56e85d332b9af39a6b1a5175b97f30dd884fa528
enesudimov <enesudimov@baltbet.ru>
2022-06-01 12:50:54 +0300

    Add invoke initDB.sql to UpdateApkServerAdmin

--- resources/windows/Deploy/UpdateApkServiceAdmin/UpdateApkServiceAdmin.ps1
+++ resources/windows/Deploy/UpdateApkServiceAdmin/UpdateApkServiceAdmin.ps1
@@ -1,6 +1,7 @@
 import-module '.\scripts\sideFunctions.psm1'
 
-$pathtojson = "C:\Services\UpdateApk\UpdateApkServiceAdmin\assets\config.json"
+$serviceFolder = "C:\Services\UpdateApk\UpdateApkServiceAdmin"
+$pathtojson = "$($serviceFolder)\assets\config.json"
 
 Write-Host -ForegroundColor Green "[info] edit json files"
 $json_config = Get-Content -Raw -path $pathtojson | % { $_ -replace '[\s^]//.*', "" } | ConvertFrom-Json 
@@ -8,3 +9,7 @@ $json_config.apiUrl = "https:\\$($env:COMPUTERNAME).bb-webapps.com:4458"
 
 ConvertTo-Json $json_config -Depth 4| Format-Json | Set-Content $pathtojson -Encoding UTF8
 Write-Host -ForegroundColor Green "$pathtojson renewed with json depth $jsonDepth"
+
+
+Write-Host -ForegroundColor Green "Invoke InitDB.sql"
+Invoke-Sqlcmd -Database UpdateApkService -InputFile "$($serviceFolder)'InitDB.sql" -
