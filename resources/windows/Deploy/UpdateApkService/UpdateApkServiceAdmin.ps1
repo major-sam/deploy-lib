96bc95bbb1e6b97562a8914380383019fbbf172d
enesudimov <enesudimov@baltbet.ru>
2022-06-01 11:30:31 +0300

    Add UpdateApkServiceAdmin deployment scripts

---
+++ resources/windows/Deploy/UpdateApkService/UpdateApkServiceAdmin.ps1
@@ -0,0 +1,10 @@
+import-module '.\scripts\sideFunctions.psm1'
+
+$pathtojson = "C:\Services\UpdateApk\UpdateApkServiceAdmin\assets\config.json"
+
+Write-Host -ForegroundColor Green "[info] edit json files"
+$json_config = Get-Content -Raw -path $pathtojson | % { $_ -replace '[\s^]//.*', "" } | ConvertFrom-Json 
+$json_config.apiUrl = "https:\\$($env:COMPUTERNAME).bb-webapps.com:9880"
+
+ConvertTo-Json $json_config -Depth 4| Format-Json | Set-Content $pathtojson -Encoding UTF8
+Write-Host -ForegroundColor Green "$pathtojson renewed with json depth $jsonDepth"
