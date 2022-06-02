a85cf2e369ff249285c39ce69ab27ad216fedd28
vrebyachih <vrebyachih@baltbet.ru>
2022-06-02 14:03:00 +0300

    fix [BaltBetM].dbo.AccountGroupProperties vm url

--- resources/windows/Post/Kernel/ComAccounts.ps1
+++ resources/windows/Post/Kernel/ComAccounts.ps1
@@ -1,7 +1,7 @@
 $q = "
 -- Установить для всех СОМ аккунтов свойство 116 для доступа на сайт СОМ
 
-DECLARE @MyUrl nvarchar(1024); 
+DECLARE @MyUrl nvarchar(1024);
 -- copy your vm url without port and https:// (like wrote in the example ).
 SET @MyUrl = '#VM_URL.bb-webapps.com';
 
@@ -10,6 +10,10 @@ set IDENTITY_INSERT [BaltBetDomain].[dbo].[ParentDomains] ON
 insert into [BaltBetDomain].[dbo].[ParentDomains] (Id , Name, Address, IsActive, ActivationDate, State)
   VALUES (1, NULL, @MyUrl, 'True' , GETDATE(), 10)
 set IDENTITY_INSERT [BaltBetDomain].[dbo].[ParentDomains] OFF
+go
+UPDATE [BaltBetM].dbo.AccountGroupProperties
+SET PropertyValueEx = @MyUrl
+WHERE AccountPropertyTypeId like '116' and PropertyValueEx like '%VM_URL%'
 "
 
 
