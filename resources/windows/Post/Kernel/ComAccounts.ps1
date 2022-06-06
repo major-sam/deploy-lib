f51da89d8a0c4506b031ad35e1b025e7e6f79491
vrebyachih <vrebyachih@baltbet.ru>
2022-06-06 18:55:34 +0300

    fix

--- resources/windows/Post/Kernel/ComAccounts.ps1
+++ resources/windows/Post/Kernel/ComAccounts.ps1
@@ -2,20 +2,19 @@ $pcname= "$env:COMPUTERNAME".ToLower()
 $query = "
 -- Установить для всех СОМ аккунтов свойство 116 для доступа на сайт СОМ
 
-DECLARE @MyUrl nvarchar(1024);
+DECLARE @MyUrl nvarchar(1024)
 
-SET @MyUrl = N'$pcname.bb-webapps.com';
+SET @MyUrl = N'$($pcname).bb-webapps.com'
 
 set IDENTITY_INSERT [BaltBetDomain].[dbo].[ParentDomains] ON
 insert into [BaltBetDomain].[dbo].[ParentDomains] (Id , Name, Address, IsActive, ActivationDate, State)
   VALUES (1, NULL, @MyUrl, 'True' , GETDATE(), 10)
 set IDENTITY_INSERT [BaltBetDomain].[dbo].[ParentDomains] OFF
-go
 UPDATE [BaltBetM].dbo.AccountGroupProperties
 SET PropertyValueEx = @MyUrl
 WHERE AccountPropertyTypeId like '116' and PropertyValueEx like '%#VM_URL%'
+go
 "
 
-
 Write-Host -ForegroundColor Green "[INFO] Execute COMAccounts.sql on BaltBetDomain"
-Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Query $query -ErrorAction continue
+Invoke-Sqlcmd -verbose -QueryTimeout 720 -ServerInstance $env:COMPUTERNAME -Query $query
