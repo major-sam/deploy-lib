9fdec97b1203f29416f7ca913a1049b34ca95ca2
syudin <syudin@baltbet.ru>
2022-06-14 13:25:40 +0300

    Update query

--- resources/windows/Deploy/Uni/UniRuSql.ps1
+++ resources/windows/Deploy/Uni/UniRuSql.ps1
@@ -2,49 +2,6 @@ Import-module '.\scripts\sideFunctions.psm1'
 
 $IPAddress = (Get-NetIPAddress -AddressFamily ipv4 |  Where-Object -FilterScript { $_.interfaceindex -ne 1}).IPAddress.trim()
 $query = "
-UPDATE [UniRu].Settings.SiteOptions SET Value = CASE Name
-WHEN 'Global.CKFinderSettings.ImageHost' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:443'
-WHEN 'Global.GlobalLog.BaltBetClientStatistics.StatisticsHandlerUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:13443/st'
-WHEN 'Global.GlobalLog.RabbitMq.DefaultConnectionString' THEN 'host=$($ENV:RABBIT_HOST):$($ENV:RABBIT_PORT); username=$($ENV:RABBIT_CREDS_USR); password=$($ENV:RABBIT_CREDS_PSW)$($ENV:VM_ID); publisherConfirms=true; timeout=100; requestedHeartbeat=0'
-WHEN 'Global.GlobalLog.RabbitMq.GlobalLogger.ConnectionString' THEN 'host=$($ENV:RABBIT_HOST):$($ENV:RABBIT_PORT); username=$($ENV:RABBIT_CREDS_USR); password=$($ENV:RABBIT_CREDS_PSW)$($ENV:VM_ID); publisherConfirms=true; timeout=100; requestedHeartbeat=0'
-WHEN 'Global.RabbitMq.AccountBus.ConnectionString' THEN 'host=$($ENV:RABBIT_HOST):$($ENV:RABBIT_PORT); username=$($ENV:RABBIT_CREDS_USR); password=$($ENV:RABBIT_CREDS_PSW)$($ENV:VM_ID); publisherConfirms=true; timeout=100; requestedHeartbeat=0'
-WHEN 'Global.KernelRedisConnectionString' THEN '$($ENV:REDIS_HOST):$($ENV:REDIS_PORT),password=$($ENV:REDIS_CREDS_PSW)$($ENV:VM_ID),syncTimeout=10000,allowAdmin=True,connectTimeout=10000,ssl=False,abortConnect=False,connectRetry=10,proxy=None'
-WHEN 'PlayerIdentificationSettings.DocumentUploadSettings.RecognitionCompletingPassportAddress' THEN 'http://localhost:8123/api/AccountFiles/Cps/completingPassportData/{0}'
-WHEN 'PlayerIdentificationSettings.DocumentUploadSettings.RecognitionResultsAddress' THEN 'http://localhost:8123/api/AccountFiles/Cps/passports/{0}'
-WHEN 'PlayerIdentificationSettings.DocumentUploadSettings.UploadingDocumentAddress' THEN 'http://localhost:8123/api/AccountFiles/Cps/Upload/{0}/{1}/{2}/{3}'
-WHEN 'Global.MessagesServiceSettings.URL' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:4442/ '
-WHEN 'Global.SignalR.Remote.Endpoint' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:8491/'
-WHEN 'Registration.InSessionDataEncrptionPassphrase' THEN 'Qwerty1z'
-WHEN 'SupportContacts.SupportEmail' THEN 'report@baltbet.ru'
-WHEN 'UpdateApk.AndroidUrl' THEN 'https://apkupdater.baltbet.ru:1415/com.baltbet.clientapp'
-WHEN 'UpdateApk.CheckUrl' THEN 'https://apkupdater-test.bb-webapps.com:1443/api/check/'
-WHEN 'Global.WcfClient.WcfServicesHostAddress' THEN '$($IPAddress)'
-WHEN 'OAuth.LastLogoutUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:449/account/logout/last'
-WHEN 'OAuth.TokenUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:449/oauth/token'
-WHEN 'Global.RabbitMq.NotificationGateWayBus.IsEnabled' THEN 'false'
-WHEN 'Payment.IsCupisPaymentsEnabled' THEN 'true'
-WHEN N'PlayerIdentificationSettings.ECupisAddressЕСИА' THEN 'https://wallet.1cupis.ru/auth'
-WHEN 'Asterisk.IpAddress' THEN '172.16.0.54'
-WHEN 'Asterisk.Port' THEN '5038'
-WHEN 'Asterisk.Login' THEN 'site'
-WHEN 'Asterisk.Secret' THEN '$($env:AsteriskSecret)'
-WHEN 'Pages.Prematch.IsHotEventsEnabled' THEN 'true'
-WHEN 'Pages.Events.IsMarketsClientSideRenderingEnabled' THEN 'true'
-WHEN 'Global.RemoteWebApi.PrematchService.Uri' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:4435'
-WHEN 'PlayerIdentificationSettings.WrongAttemptCount' THEN '5'
-WHEN 'BroadcastSettings.SetkaCup.StreamUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:4443/broadcast/setkacup/{matchId}'
-WHEN 'BroadcastSettings.MatchTv.StreamUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:4443/dm-mobileapp/broadcast/matchtv/{matchId}'
-WHEN 'BroadcastSettings.Rfpl.StreamUrl' THEN 'https://$($env:computername.ToLower()).bb-webapps.com:4443/dm-mobileapp/broadcast/umamedia/{matchId}'
-WHEN 'Global.RabbitMq.PicBus.ConnectionString' THEN 'host=$($ENV:RABBIT_HOST):$($ENV:RABBIT_PORT); username=$($ENV:RABBIT_CREDS_USR); password=$($ENV:RABBIT_CREDS_PSW)$($ENV:VM_ID); publisherConfirms=true; timeout=100; requestedHeartbeat=0'
-WHEN 'Global.RabbitMq.PicBus.IsEnabled' THEN 'true'
-WHEN 'Global.RabbitMq.PicBus.Exchange' THEN 'Exchange.Pic.Ru'	
-
-ELSE Value END
-GO
-
-DELETE FROM UniRu.Settings.SiteOptions WHERE NAME like '%RemoteWebApi%'
-
-
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'PreRegistrationData')
 BEGIN
 	DROP TABLE dbo.PreRegistrationData;
