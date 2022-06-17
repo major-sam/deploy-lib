c7eb48419d18f64f65b5a3b750a76a91e7ca1379
enesudimov <enesudimov@baltbet.ru>
2022-06-17 13:29:02 +0300

    Add BaltWidgetsClient settings

---
+++ resources/windows/DeployWidgets/WidgetService/BaltWidgetsClient.ps1
@@ -0,0 +1,23 @@
+Import-module '.\scripts\sideFunctions.psm1'
+
+$serviceName = "BaltWidgetsClient"
+$targetDir = "C:\Services\BaltWidgets\${serviceName}"
+$ProgressPreference = 'SilentlyContinue'
+$pathtojson = "$targetDir\api.host.json"
+$wildcardDomain = "bb-webapps.com"
+$jsonDepth = 5
+
+
+if (Test-path($pathtojson)) {
+    Write-host "[INFO] Start ${serviceName} deploy script"
+    Write-Host -ForegroundColor Green "[INFO] Edit $pathtojson"
+    $jsonAppsetings = Get-Content -Raw -path $pathtojson  | % { $_ -replace '[\s^]//.*', "" } | ConvertFrom-Json 
+
+    $jsonAppsetings.BackendUrl = "https://$($env:COMPUTERNAME).$($wildcardDomain):6001"
+    
+    ConvertTo-Json $jsonAppsetings -Depth $jsonDepth  | Format-Json | Set-Content $pathtojson -Encoding UTF8
+    Write-Host -ForegroundColor Green "[INFO] $pathtojson renewed with json depth $jsonDepth"
+}
+else {
+    Write-host "[INFO] $pathtojson does not exist. Skip deploy $serviceName."
+}
