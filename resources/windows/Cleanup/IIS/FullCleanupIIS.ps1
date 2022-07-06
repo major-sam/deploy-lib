4166c04c15acd798a2422b04a18bf63f82bd3ff0
enesudimov <enesudimov@baltbet.ru>
2022-07-06 10:58:00 +0300

    Added removing iis logs in cleanup

--- resources/windows/Cleanup/IIS/FullCleanupIIS.ps1
+++ resources/windows/Cleanup/IIS/FullCleanupIIS.ps1
@@ -3,6 +3,12 @@ Import-Module  -Force WebAdministration
 Remove-Website -Name *
 Remove-WebAppPool -name *
 Stop-Service W3SVC
-Get-ChildItem 'C:\inetpub'  -Exclude custerr, history, logs, temp, wwwroot  | %{ 
-    "Removing file $($_.FullName)";Remove-Item -Force -Recurse -Path $_}
+Get-ChildItem 'C:\inetpub'  -Exclude custerr, history, temp, wwwroot  | %{ 
+    "Removing files in $($_.FullName)"
+    if ($_.Name -eq "logs") {
+        Remove-Item -Force -Recurse -Path "$($_)\*" 
+        return
+    }
+    Remove-Item -Force -Recurse -Path $_.FullName
+}
 Start-Service W3SVC
