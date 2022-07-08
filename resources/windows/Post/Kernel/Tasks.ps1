fcc22fe724e54579998bd86446055f9e02eebdc4
syudin <syudin@baltbet.ru>
2022-07-08 16:37:09 +0300

    Disable ApplyTasks on BaltBetMMirror database

--- resources/windows/Post/Kernel/Tasks.ps1
+++ resources/windows/Post/Kernel/Tasks.ps1
@@ -2,4 +2,4 @@ Import-module '.\scripts\sideFunctions.psm1'
 
 $folder =  "C:\Kernel\TasksDB"
 ApplyTasks -ScriptFolder $folder -TargetDB 'BaltBetM' -DBServer $env:COMPUTERNAME
-ApplyTasks -ScriptFolder $folder -TargetDB  'BaltBetMMirror'-DBServer $env:COMPUTERNAME
+#ApplyTasks -ScriptFolder $folder -TargetDB  'BaltBetMMirror'-DBServer $env:COMPUTERNAME
