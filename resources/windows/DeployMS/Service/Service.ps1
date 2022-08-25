e5e76a30d23b287ef211650b3c17910b0c684c07
vrebyachih <vrebyachih@baltbet.ru>
2022-08-25 17:11:20 +0300

    update marketing service config

--- resources/windows/DeployMS/Service/Service.ps1
+++ resources/windows/DeployMS/Service/Service.ps1
@@ -24,7 +24,10 @@ $jsonAppsetings = Get-Content -Raw -path $pathtojson  | % {$_ -replace  '[\s^]//
 $fileLogs = $jsonAppsetings.Serilog.WriteTo | where {$_.Name -eq 'File' }
 $fileLogs.Args.path = "c:\Logs\MarketingService\MarketingService.log"
 $jsonAppsetings.FilesService.UploadFolderPath = "C:\inetpub\MarketingImages"
-$jsonAppsetings.FilesService.PublicationBaseUrl = "https://$($env:COMPUTERNAME).$($defaultDomain):9883"
+$jsonAppsetings.FilesService.PublicationBaseUrl = "https://$($env:COMPUTERNAME).$($defaultDomain):9883".ToLower()
+$jsonAppsetings.UrlConfig.newsUrl = "https://$($env:COMPUTERNAME).$($defaultDomain):9882/desktop/info/news".ToLower()
+$jsonAppsetings.UrlConfig.promotionRulesUrl = "https://$($env:COMPUTERNAME).$($defaultDomain):9882/desktop/static-page/promotion-rules".ToLower()
+$jsonAppsetings.UrlConfig.linkAccountToUniUrl = "https://$($env:COMPUTERNAME).$($defaultDomain):9882/marketing/authorize?data=".ToLower()
 $jsonAppsetings.connectionStrings.RabbitMQ = $shortRabbitStr
 ConvertTo-Json $jsonAppsetings -Depth $jsonDepth  | Format-Json | Set-Content $pathtojson -Encoding UTF8
 Write-Host -ForegroundColor Green "$pathtojson renewed with json depth $jsonDepth"
