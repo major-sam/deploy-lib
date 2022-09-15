b83edb74718f3cef13f19b6632c467e1d39c858e
vrebyachih <vrebyachih@baltbet.ru>
2022-09-15 16:38:11 +0300

    remove Website

--- resources/windows/Deploy/IIS/IISConfig.ps1
+++ resources/windows/Deploy/IIS/IISConfig.ps1
@@ -62,20 +62,6 @@ $IISPools = @(
 		siteSubDir = $true
     }
     @{
-        SiteName = 'Website'
-        RuntimeVersion = 'v4.0'
-        DomainAuth =  @{
-            userName="$username";password="$pass";identitytype=3
-            }
-        Bindings= @(
-                @{protocol='http';bindingInformation="*:81:$($env:COMPUTERNAME).$($wildcardDomain)"}
-                @{protocol='https';bindingInformation="*:4445:$($env:COMPUTERNAME).$($wildcardDomain)"}
-            )
-		CertPath = 'Cert:\LocalMachine\My\38be86bcf49337804643a671c4c56bc4224c6606'
-		rootDir = 'c:\inetpub'
-		siteSubDir = $true
-    }
-    @{
         SiteName = 'KRM'
         RuntimeVersion = 'v4.0'
         DomainAuth =  @{
