7fc091f49fb402b1b1ad848fc1f8d2cb8c03df9f
vrebyachih <vrebyachih@baltbet.ru>
2022-09-19 15:54:33 +0300

    back to iis

--- resources/windows/Post/UniPaymentsService/UniPaymentsService.ps1
+++ resources/windows/Post/UniPaymentsService/UniPaymentsService.ps1
@@ -3,6 +3,6 @@ Import-module '.\scripts\sideFunctions.psm1'
 
 # Регистрируем сервис
 $serviceBin = Get-Item  "C:\Services\UniPaymentsService\Uni.PaymentsService.exe"
-$sname = RegisterWinService($serviceBin)
-Start-Service $sname
-Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
+#$sname = RegisterWinService($serviceBin)
+#Start-Service $sname
+#Set-Recovery -ServiceDisplayName $sname -Server $env:COMPUTERNAME
