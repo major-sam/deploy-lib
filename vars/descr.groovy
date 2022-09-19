3feb2d8393f4afe411b7188a9f3694487aab556e
vrebyachih <vrebyachih@baltbet.ru>
2022-09-19 11:26:16 +0300

    fix null descr

--- vars/descr.groovy
+++ vars/descr.groovy
@@ -1,6 +1,6 @@
 def setAllowDescr(Map config = [:]){
 	def BUILD_TRIGGER_BY = config.couse.shortDescription + 
-		"/" + config.couse.userId
+		(config.couse.userId ? "/" + config.couse.userId : '')
 
 	return (currentBuild.description ? currentBuild.description +'<br/>' : '') +
 		"vm: ${config.vmName}<br/>${BUILD_TRIGGER_BY}" +
