f574ef9a11ec044b025f39da56434c4bd57a7cf6
syudin <syudin@baltbet.ru>
2022-09-26 16:51:00 +0300

    Add OpenSSH install

---
+++ resources/windows/Deploy/conf/openSSH.ps1
@@ -0,0 +1,8 @@
+# Install OpenSSH
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
